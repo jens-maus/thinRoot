4792c4ddeeb0b08ecbc4fd3c2e4bd7823e1bfb91
Jens Maus <mail@jens-maus.de>
2019-06-26 13:49:57 +0200

    let umounter.sh umount everything mounted in /media/usbX

--- buildroot-external/rootfs-overlay/bin/umounter.sh
+++ buildroot-external/rootfs-overlay/bin/umounter.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-for MNTPATH in `grep /mnt /proc/mounts | awk '{ print $2 }'`; do
+for MNTPATH in $(grep /media/usb /proc/mounts | awk '{ print $2 }'); do
   /bin/umount ${MNTPATH}
-  if [ $? -eq 0 ] ;then 
-    CHECK=`grep ${MNTPATH} /proc/mounts`
-    if [ $? -ne 0 ] ;then
-      rm -r ${MNTPATH}
-    fi
-  fi
 done
