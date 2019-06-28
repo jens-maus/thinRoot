4e32e25d58560136ae1e4437cd4e244d87be4b5d
Jens Maus <mail@jens-maus.de>
2019-06-28 12:04:21 +0200

    remove pulseaudio-system.conf from global dbus configuration as this is not required.

--- buildroot-external/board/intel_nuc/post-build.sh
+++ buildroot-external/board/intel_nuc/post-build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# remove /etc/dbus-1/system.d/pulseaudio-system.conf
+rm -f ${TARGET_DIR}/etc/dbus-1/system.d/pulseaudio-system.conf
+
 # remove /etc/init.d/S40xorg as not needed
 rm -f ${TARGET_DIR}/etc/init.d/S40xorg
