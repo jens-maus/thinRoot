0a65298a267d42fbfa1bab04a193f1ae7155650d
Jens Maus <mail@jens-maus.de>
2021-07-20 09:35:48 +0200

    modified thinlinc package to use the different platform packages depending on the CPU platform we are using. Unfortunately ThinLinc is not available for aarch64 yet, thus we only enable it for the armhf (32bit) ARM platforms.

--- buildroot-external/package/thinlinc/thinlinc.mk
+++ buildroot-external/package/thinlinc/thinlinc.mk
@@ -7,8 +7,15 @@
 ################################################################################
 
 THINLINC_VERSION = 4.12.1-6733
-THINLINC_SOURCE = tl-$(THINLINC_VERSION)-client-linux-dynamic-x86_64.tar.gz
 THINLINC_SITE = https://www.cendio.com/downloads/clients
+
+ifeq ($(call qstrip,$(BR2_ARCH)),x86_64)
+THINLINC_SOURCE = tl-$(THINLINC_VERSION)-client-linux-dynamic-x86_64.tar.gz
+else ifeq ($(call qstrip,$(BR2_ARCH)),i686)
+THINLINC_SOURCE = tl-$(THINLINC_VERSION)-client-linux-dynamic-i686.tar.gz
+else ifeq ($(call qstrip,$(BR2_ARCH)),arm)
+THINLINC_SOURCE = tl-$(THINLINC_VERSION)-client-linux-dynamic-armhf.tar.gz
+endif
 
 define THINLINC_INSTALL_TARGET_CMDS
   cp -R $(@D)/bin/* $(TARGET_DIR)/bin/
