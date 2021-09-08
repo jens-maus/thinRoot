e77345bc1633fe5109a6b619e08ca1eb5b252163
Jens Maus <mail@jens-maus.de>
2021-09-08 09:54:23 +0200

    add default bootEnv.txt files for rpi and tinkerboard platform but with disabled entries so that users can pickup the BASE_PATH stuff on their own more easily.

--- buildroot-external/board/rpi4/post-image.sh
+++ buildroot-external/board/rpi4/post-image.sh
@@ -6,6 +6,7 @@ BOARD_NAME="$(basename "${BOARD_DIR}")"
 # Use our own cmdline.txt+config.txt
 cp "${BR2_EXTERNAL_THINROOT_PATH}/board/${BOARD_NAME}/cmdline.txt" "${BINARIES_DIR}/"
 cp "${BR2_EXTERNAL_THINROOT_PATH}/board/${BOARD_NAME}/config.txt" "${BINARIES_DIR}/"
+cp "${BR2_EXTERNAL_THINROOT_PATH}/board/${BOARD_NAME}/bootEnv.txt" "${BINARIES_DIR}/"
 
 # VERSION File
 cp "${TARGET_DIR}/boot/VERSION" "${BINARIES_DIR}"
--- buildroot-external/board/tinkerboard/post-image.sh
+++ buildroot-external/board/tinkerboard/post-image.sh
@@ -4,9 +4,10 @@
 BOARD_DIR="$(dirname "$0")"
 BOARD_NAME="$(basename "${BOARD_DIR}")"
 
-#
+# bootEnv.txt file
+cp "${BR2_EXTERNAL_THINROOT_PATH}/board/${BOARD_NAME}/bootEnv.txt" "${BINARIES_DIR}/"
+
 # VERSION File
-#
 cp "${TARGET_DIR}/boot/VERSION" "${BINARIES_DIR}"
 
 # create *.img file using genimage
