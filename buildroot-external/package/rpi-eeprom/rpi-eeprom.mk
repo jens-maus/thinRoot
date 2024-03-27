10d5827e2b1e9f0e0029d600645b5f87d99f4326
Jens Maus <j.maus@hzdr.de>
2024-03-27 17:04:23 +0100

    add update-rpi-eeprom.sh update script and update rpi-eeprom to latest version.

--- buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
+++ buildroot-external/package/rpi-eeprom/rpi-eeprom.mk
@@ -4,12 +4,19 @@
 #
 ################################################################################
 
-RPI_EEPROM_VERSION = 83347f3968200a031b3aaa4e49ed445304a1c4a0
+RPI_EEPROM_VERSION = b745226b41ac202976ee8307fcb179a1193fab3c
 RPI_EEPROM_SITE = $(call github,raspberrypi,rpi-eeprom,$(RPI_EEPROM_VERSION))
 RPI_EEPROM_LICENSE = BSD-3-Clause
 RPI_EEPROM_LICENSE_FILES = LICENSE
 RPI_EEPROM_INSTALL_IMAGES = YES
-RPI_EEPROM_FIRMWARE_PATH = firmware/stable/pieeprom-2022-04-26.bin
+
+ifeq ($(BR2_PACKAGE_RPI_EEPROM_RPI4),y)
+  # Raspberry Pi 4
+  RPI_EEPROM_FIRMWARE_PATH = firmware-2711/stable/pieeprom-2024-01-22.bin
+else ifeq ($(BR2_PACKAGE_RPI_EEPROM_RPI5),y)
+  # Raspberry Pi 5
+  RPI_EEPROM_FIRMWARE_PATH = firmware-2712/stable/pieeprom-2024-02-16.bin
+endif
 
 define RPI_EEPROM_BUILD_CMDS
 	$(@D)/rpi-eeprom-config $(@D)/$(RPI_EEPROM_FIRMWARE_PATH) --out $(@D)/default.conf
