07a41682e6e55545eeef88423e3c042c0e181a4a
Jens Maus <mail@jens-maus.de>
2024-07-25 17:24:47 +0200

    rework dependency list

--- buildroot-external/package/spice-gtk/spice-gtk.mk
+++ buildroot-external/package/spice-gtk/spice-gtk.mk
@@ -13,7 +13,18 @@ SPICE_GTK_LICENSE = GPL-2.0
 SPICE_GTK_LICENSE_FILES = COPYING
 SPICE_GTK_INSTALL_STAGING = YES
 
-SPICE_GTK_DEPENDENCIES = spice-protocol host-python-six host-python-pyparsing jpeg json-glib gstreamer1 gst1-plugins-base usbredir usbutils phodav opus
+SPICE_GTK_DEPENDENCIES = \
+	spice-protocol \
+	host-python-six \
+	host-python-pyparsing \
+	jpeg \
+	json-glib \
+	gstreamer1 \
+	gst1-plugins-base \
+	usbredir \
+	usbutils \
+	phodav \
+	opus
 
 SPICE_GTK_CONF_OPTS += -Dusb-ids-path=/usr/share/hwdata/usb.ids
 
