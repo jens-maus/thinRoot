6de8e2d91fa332cce453f5ab9fa37b6e1132d3fa
Jens Maus <mail@jens-maus.de>
2025-01-17 16:13:06 +0100

    make sure to remove /etc/X11/xorg.conf.d/20-intel.conf because it does not work with rpi

--- buildroot-external/board/rpi3/post-build.sh
+++ buildroot-external/board/rpi3/post-build.sh
@@ -17,3 +17,6 @@ rm -f "${TARGET_DIR}/etc/dbus-1/system.d/pulseaudio-system.conf"
 
 # remove /etc/init.d/S40xorg as not needed
 rm -f "${TARGET_DIR}/etc/init.d/S40xorg"
+
+# remove /etc/X11/xorg.conf.d/20-intel.conf as it will not work on rpi
+rm -f "${TARGET_DIR}/etc/X11/xorg.conf.d/20-intel.conf"
