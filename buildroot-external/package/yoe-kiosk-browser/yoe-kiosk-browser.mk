51aab6253cc721117a9075b411dd3d8608c7e4d0
Jens Maus <mail@jens-maus.de>
2025-01-20 09:48:40 +0100

    update yoe-kiosk-browser to have correct dependencies - however, it still does not build because of missing qt6webengine/qt6webview in buildroot.

--- buildroot-external/package/yoe-kiosk-browser/yoe-kiosk-browser.mk
+++ buildroot-external/package/yoe-kiosk-browser/yoe-kiosk-browser.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-YOE_KIOSK_BROWSER_VERSION = 610be9b29fb348576264ff2e44a584c152f3f390
+YOE_KIOSK_BROWSER_VERSION = 47846c7a62aea8317070f4a79b80cd26ded74bdd
 YOE_KIOSK_BROWSER_SITE = https://github.com/YoeDistro/yoe-kiosk-browser
 YOE_KIOSK_BROWSER_SITE_METHOD = git
-YOE_KIOSK_BROWSER_DEPENDENCIES = qt6base
+YOE_KIOSK_BROWSER_DEPENDENCIES = qt6base qt6declarative qt6virtualkeyboard
 YOE_KIOSK_BROWSER_LICENSE = LGPL-3.0
 
 #define QT_WEBENGINE_KIOSK_CONFIGURE_CMDS
