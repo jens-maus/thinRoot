d005945fedf34d7a6865bc468451b7a231a7f0c8
Jens Maus <mail@jens-maus.de>
2025-01-30 15:04:13 +0100

    enable basic RaspberryPi4 build environment support.

--- buildroot-external/board/rpi4/post-build.sh
+++ buildroot-external/board/rpi4/post-build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Stop on error
+set -e
+
 # create VERSION file
 echo "VERSION=${PRODUCT_VERSION}" >"${TARGET_DIR}/VERSION"
 echo "PRODUCT=${PRODUCT}" >>"${TARGET_DIR}/VERSION"
@@ -14,3 +17,6 @@ rm -f "${TARGET_DIR}/etc/dbus-1/system.d/pulseaudio-system.conf"
 
 # remove /etc/init.d/S40xorg as not needed
 rm -f "${TARGET_DIR}/etc/init.d/S40xorg"
+
+# remove /etc/X11/xorg.conf.d/20-intel.conf as it will not work on rpi
+rm -f "${TARGET_DIR}/etc/X11/xorg.conf.d/20-intel.conf"
--- buildroot-external/board/rpi4/post-image.sh
+++ buildroot-external/board/rpi4/post-image.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
+# Stop on error
+set -e
+
 BOARD_DIR="$(dirname "$0")"
-BOARD_NAME="$(basename "${BOARD_DIR}")"
+#BOARD_NAME="$(basename "${BOARD_DIR}")"
 
 # Use our own cmdline.txt+config.txt
-cp "${BR2_EXTERNAL_THINROOT_PATH}/board/${BOARD_NAME}/cmdline.txt" "${BINARIES_DIR}/"
-cp "${BR2_EXTERNAL_THINROOT_PATH}/board/${BOARD_NAME}/config.txt" "${BINARIES_DIR}/"
-cp "${BR2_EXTERNAL_THINROOT_PATH}/board/${BOARD_NAME}/bootEnv.txt" "${BINARIES_DIR}/"
+cp "${BOARD_DIR}/cmdline.txt" "${BINARIES_DIR}/"
+cp "${BOARD_DIR}/config.txt" "${BINARIES_DIR}/"
+cp "${BOARD_DIR}/bootEnv.txt" "${BINARIES_DIR}/"
+
+# README needs to be present, otherwise os_prefix is not
+# prepended implicitly to the overlays' path, see:
+# https://www.raspberrypi.com/documentation/computers/config_txt.html#overlay_prefix
+touch "${BINARIES_DIR}/overlays/README" 2>/dev/null || true
 
 # VERSION File
 cp "${TARGET_DIR}/boot/VERSION" "${BINARIES_DIR}"
 
 # create *.img file using genimage
-support/scripts/genimage.sh -c "${BR2_EXTERNAL_THINROOT_PATH}/board/${BOARD_NAME}/genimage.cfg"
-
-exit $?
+support/scripts/genimage.sh -c "${BOARD_DIR}/genimage.cfg"
