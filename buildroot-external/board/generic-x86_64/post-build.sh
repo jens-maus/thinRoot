98b0340646e295d03bb452e17747a3e932da81d2
Jens Maus <mail@jens-maus.de>
2025-02-04 14:49:22 +0100

    make sure to remove unnecessary target files from post-build.sh to reduce number of bootup errors/warnings.

--- buildroot-external/board/generic-x86_64/post-build.sh
+++ buildroot-external/board/generic-x86_64/post-build.sh
@@ -7,6 +7,13 @@ echo "PLATFORM=generic-x86_64" >>"${TARGET_DIR}/VERSION"
 
 # remove /etc/dbus-1/system.d/pulseaudio-system.conf
 rm -f "${TARGET_DIR}/etc/dbus-1/system.d/pulseaudio-system.conf"
+rm -f "${TARGET_DIR}/usr/share/dbus-1/system.d/pulseaudio-system.conf"
 
 # remove /etc/init.d/S40xorg as not needed
 rm -f "${TARGET_DIR}/etc/init.d/S40xorg"
+
+# remove /lib/dhcpcd/dhcpcd-hooks/50-timesyncd.conf as it is systemd only
+rm -f "${TARGET_DIR}/lib/dhcpcd/dhcpcd-hooks/50-timesyncd.conf"
+
+# remove unnecessary /etc/init.d/fuse3
+rm -f "${TARGET_DIR}/etc/init.d/fuse3"
--- buildroot-external/board/tinkerboard/post-build.sh
+++ buildroot-external/board/tinkerboard/post-build.sh
@@ -14,9 +14,16 @@ ln -sf ../VERSION "${TARGET_DIR}/boot/VERSION"
 
 # remove /etc/dbus-1/system.d/pulseaudio-system.conf
 rm -f "${TARGET_DIR}/etc/dbus-1/system.d/pulseaudio-system.conf"
+rm -f "${TARGET_DIR}/usr/share/dbus-1/system.d/pulseaudio-system.conf"
 
 # remove /etc/init.d/S40xorg as not needed
 rm -f "${TARGET_DIR}/etc/init.d/S40xorg"
 
 # remove /etc/X11/xorg.conf.d/20-intel.conf as it will not work on rpi
 rm -f "${TARGET_DIR}/etc/X11/xorg.conf.d/20-intel.conf"
+
+# remove /lib/dhcpcd/dhcpcd-hooks/50-timesyncd.conf as it is systemd only
+rm -f "${TARGET_DIR}/lib/dhcpcd/dhcpcd-hooks/50-timesyncd.conf"
+
+# remove unnecessary /etc/init.d/fuse3
+rm -f "${TARGET_DIR}/etc/init.d/fuse3"
