de46cfc831ac206a7c9f70943bf6dba7ad57774a
Jens Maus <mail@jens-maus.de>
2026-01-21 15:47:18 +0100

    updated qutselect to 3.14 with updated app.sh startup script for better BBB integration.

--- buildroot-external/package/qutselect/qutselect.mk
+++ buildroot-external/package/qutselect/qutselect.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QUTSELECT_VERSION = 3.13
+QUTSELECT_VERSION = 3.14
 QUTSELECT_SITE = $(call github,hzdr,qutselect,$(QUTSELECT_VERSION))
 QUTSELECT_LICENSE = LGPL-3.0
 QUTSELECT_LICENSE_FILES = LICENSE
